5ed1883774dd0da2474823814183df02df4789fe
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2017-02-13 08:57:41 +0100

    added the logic for the IdealoDE export plugin

---
+++ src/ElasticExportIdealoDEServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ElasticExportIdealoDE;
+
+use Plenty\Modules\DataExchange\Services\ExportPresetContainer;
+use Plenty\Plugin\DataExchangeServiceProvider;
+
+class ElasticExportIdealoDEServiceProvider extends DataExchangeServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function exports(ExportPresetContainer $container)
+    {
+        $container->add(
+            'IdealoDE-Plugin',
+            'ElasticExportIdealoDE\ResultField\IdealoDE',
+            'ElasticExportIdealoDE\Generator\IdealoDE',
+            'ElasticExportIdealoDE\Filter\IdealoDE',
+            true
+        );
+    }
+}
